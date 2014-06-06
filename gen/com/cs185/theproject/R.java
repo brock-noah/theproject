9233e1ba8ba90e88d4d16530b015b3c3fb4f1d6e
Brock Noah <brocknoah@gmail.com>
2014-06-06 13:50:44 -0700

    added info dialog

--- gen/com/cs185/theproject/R.java
+++ gen/com/cs185/theproject/R.java
@@ -985,17 +985,18 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
-        public static final int minus=0x7f020058;
-        public static final int plus=0x7f020059;
-        public static final int welcome=0x7f02005a;
-        public static final int welcome0=0x7f02005b;
-        public static final int welcome1=0x7f02005c;
+        public static final int info=0x7f020058;
+        public static final int minus=0x7f020059;
+        public static final int plus=0x7f02005a;
+        public static final int welcome=0x7f02005b;
+        public static final int welcome0=0x7f02005c;
+        public static final int welcome1=0x7f02005d;
     }
     public static final class id {
         public static final int ButtonViewCart=0x7f050048;
         public static final int ListViewCatalog=0x7f050047;
         public static final int TextView01=0x7f050046;
-        public static final int action_account=0x7f050053;
+        public static final int action_account=0x7f050055;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1003,13 +1004,13 @@ containing a value of this type.
         public static final int action_bar_root=0x7f05001a;
         public static final int action_bar_subtitle=0x7f050023;
         public static final int action_bar_title=0x7f050022;
-        public static final int action_cart=0x7f050056;
+        public static final int action_cart=0x7f050058;
         public static final int action_context_bar=0x7f05001d;
-        public static final int action_help=0x7f050055;
+        public static final int action_help=0x7f050057;
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050054;
+        public static final int action_settings=0x7f050056;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1031,10 +1032,12 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageButton1=0x7f050050;
-        public static final int imageButton2=0x7f050051;
+        public static final int imageButton1=0x7f050051;
+        public static final int imageButton2=0x7f050052;
+        public static final int imageButton3=0x7f050054;
         public static final int imageView1=0x7f05003f;
         public static final int lblListHeader=0x7f05004e;
+        public static final int lblListInfo=0x7f050050;
         public static final int lblListItem=0x7f05004f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
@@ -1068,7 +1071,7 @@ containing a value of this type.
         public static final int subtotal=0x7f05004b;
         public static final int subtotal_value=0x7f05004c;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050052;
+        public static final int textView1=0x7f050053;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int tv_app_name=0x7f05003d;
